b614b959f6cc3e98594d20a0f208470d3d5956c6
majki <michal.rymarski2003@gmail.com>
2026-09-09 19:55:38 +0200

    fix: limit html viewport and make wasm scale properly

--- webApp/src/wasmJsMain/kotlin/prayit/simplebudget/webapp/main.kt
+++ webApp/src/wasmJsMain/kotlin/prayit/simplebudget/webapp/main.kt
@@ -2,23 +2,18 @@ package prayit.simplebudget.webapp
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 import prayit.simplebudget.App
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
-        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Box(Modifier.width(400.dp).height(850.dp)) {
-                App()
-            }
+    val container = document.getElementById("app") ?: document.body!!
+    ComposeViewport(container) {
+        Box(Modifier.fillMaxSize()) {
+            App()
         }
     }
 }

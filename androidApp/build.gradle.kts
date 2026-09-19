8d69a7f74ae291e358b4a3580dbdded3cf62ada1
majki <michal.rymarski2003@gmail.com>
2026-09-19 23:20:50 +0200

    test: unify on kotlin.test runner
    androidApp test now uses kotlin.test.Test (via kotlin-test-junit) like all KMP tests. Everything still executes on JUnit 4.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -93,4 +93,5 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
     testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
 }
